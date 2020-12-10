6084d17b2ccf1265577133fc2a064ebe196331ed
slhome2010 <slhome2010@mail.ru>
2020-12-10 17:47:08 +0600

    Modal use global window toggler

--- ES6_project/src/components/AttributeForm.jsx
+++ ES6_project/src/components/AttributeForm.jsx
@@ -2,64 +2,123 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
 
-// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-//Modal.setAppElement('#attribute-form')
-//Modal.setAppElement(document.querySelector( '#attribute-form' ));
-function AttributeForm(props){
+class AttributeForm extends React.Component {
+
+  // Lifecycle methods
+
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    }
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  componentDidMount() {
+    window.modalAttached = true;
+
+    window.toggleModal = this.toggle;
+  }
+
+  componentWillUnmount() {
+    // Есть смысл убрать отвязать обработчики обычных DOM элементов, 
+    // связанных с этим компонентом, если их много, или вы собираетесь постоянно
+    // создавать и удалять элемент
+  }
+
+
+  // Utility methods
+
+  toggle() {
+    this.setState({
+      modalIsOpen: !this.state.modalIsOpen
+    });
+  }
+
+  render() {
+    //var modalSate = this.state.modalIsOpen ? 'fade-in' : 'fade-out';
+    var subtitle;
+    return (
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        //onAfterOpen={afterOpenModal}
+        onRequestClose={this.toggle}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+
+        <h2>Hello</h2>
+        <button onClick={this.toggle}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+
+/* function AttributeForm(props) {
   var subtitle;
-  const [modalIsOpen,setIsOpen] = useState(props.modalIsOpen);
- // const { modalIsOpen, closeModal } = props;
+  const [modalIsOpen, setIsOpen] = useState(props.modalIsOpen);
+  // const { modalIsOpen, closeModal } = props;
   function openModal() {
     setIsOpen(true);
   }
-console.log(modalIsOpen)
+  console.log(modalIsOpen)
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
     subtitle.style.color = '#f00';
   }
 
-  function closeModal(){
+  function closeModal() {
     setIsOpen(false);
-  }
-/* if (props.modalIsOpen) {
-  openModal()
-} */
-    return (
-      <div>
-        
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
+  } */
+  /* if (props.modalIsOpen) {
+    openModal()
+  } */
+ /*  return (
+    <div>
 
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
-      </div>
-    );
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+
+        <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
 }
- 
+ */
 export default AttributeForm;
 //const domContainer = document.querySelector('#like_button_container');
 //ReactDOM.render(e(LikeButton), domContainer);
