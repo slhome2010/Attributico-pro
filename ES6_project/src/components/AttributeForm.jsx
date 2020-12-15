71c3c4fd9d2a493ac94ae6fcebe62952bd90ae3e
slhome2010 <slhome2010@mail.ru>
2020-12-15 17:45:14 +0600

    Modal bootstrap styled

--- ES6_project/src/components/AttributeForm.jsx
+++ ES6_project/src/components/AttributeForm.jsx
@@ -9,18 +9,18 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    padding: '15px'
+    padding: '0px'
   },
-  overlay: { 
+  overlay: {
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,  
+    bottom: 0,
     backgroundColor: 'rgba(102, 102, 102, 0.75)'
   }
 };
 
-Modal.setAppElement(document.querySelector( '#root' ))
+Modal.setAppElement(document.querySelector('#root'))
 //Modal.defaultStyles.overlay.backgroundColor = 'rgba(0, 0, 0, 0.3)'
 //Modal.defaultStyles.content.padding = '15px'
 
@@ -30,11 +30,11 @@ function AttributeForm(props) {
   // const { modalIsOpen, closeModal } = props;
   function openModal() {
     setIsOpen(true);
-  }  
+  }
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
+    //subtitle.style.color = '#f00';
   }
 
   function closeModal() {
@@ -47,9 +47,9 @@ function AttributeForm(props) {
 
   useLayoutEffect(() => {
     window.modalAttached = true;
-    window.toggleModal = toggle;    
+    window.toggleModal = toggle;
   });
-  
+
 
   return (
     <div>
@@ -59,20 +59,25 @@ function AttributeForm(props) {
         onAfterOpen={afterOpenModal}
         onRequestClose={toggle}
         style={customStyles}
-        contentLabel="Example Modal" 
+        contentLabel="Example Modal"
         shouldCloseOnOverlayClick={false}
       >
 
-        <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-        <button onClick={toggle}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title"><i className="fa fa-pencil"></i> Title </h3>
+            <button type="button" className="close" onClick={toggle}><span>&times;</span></button>
+          </div>
+          <div className="panel-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente corporis voluptas quod, aliquid minima quibusdam maxime amet nisi.
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-default" onClick={toggle}>Close</button>
+            <button type="submit" className="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+
+
       </Modal>
     </div>
   );
